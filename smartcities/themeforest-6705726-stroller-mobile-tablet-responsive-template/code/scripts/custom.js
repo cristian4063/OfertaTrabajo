c7efc7c6151a1fac0c2c25ce27ff0c33ba8c4ac6
johnedu <johnedu06@hotmail.com>
2014-09-24 22:29:15 -0500

    Online/Offline

--- smartcities/themeforest-6705726-stroller-mobile-tablet-responsive-template/code/scripts/custom.js
+++ smartcities/themeforest-6705726-stroller-mobile-tablet-responsive-template/code/scripts/custom.js
@@ -67,3 +67,24 @@ $(document).ready(function(){
 
 	
 });
+
+
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var file = "http://lavanderialabruja.com/images/logo.png";  //  Cambiar despues por una imagen del proyecto
+    var randomNum = Math.round(Math.random() * 10000);
+
+    xhr.open('HEAD', file + "?rand=" + randomNum, false);
+
+    try {
+        xhr.send();
+
+        if (xhr.status >= 200 && xhr.status < 304) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
